58dc41bdb6b7b69456f16ba1e0e373452d83c69a
violitaandriana <violitaandriana2003@gmail.com>
2025-05-31 16:00:53 +0700

    feat: boilerplate

---
+++ backend/script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# php artisan make:model PurchaseOrder -m
+# php artisan make:model PurchaseOrderDetail -m
+# php artisan make:model GoodsReceipt -m
+# php artisan make:model DebitNote -m
+
+php artisan make:controller StockCardController
+php artisan make:controller GoodsMovingController
+php artisan make:controller StockOpnameController
+php artisan make:controller ReproController
+# php artisan make:controller MasterController
+
+# php artisan make:model GoodsReceiptDetail -m
+# php artisan make:model Warehouse -m
+# php artisan make:model StockCard -m
+# php artisan make:model Cogs -m
+
+# php artisan make:model Item -m
+# php artisan make:model DebitNoteDetail -m
+# php artisan make:model SalesPrice -m
+# php artisan make:model IncomeStatement -m
+# php artisan make:model Inventory -m
+# php artisan make:model Scrap -m
+# php artisan make:model StockOpname -m
